fb5fb7325561c47dc0ed30698b41077446c5d52b
daxa2901 <daxadh24@gmail.com>
2022-04-17 15:41:38 +0530

    product module

--- app/code/local/Di/Product/Block/Adminhtml/Product/Index/Edit/Tabs.php
+++ app/code/local/Di/Product/Block/Adminhtml/Product/Index/Edit/Tabs.php
@@ -13,17 +13,8 @@ class Di_Product_Block_Adminhtml_Product_Index_Edit_Tabs extends Mage_Adminhtml_
     protected function _beforeToHtml()
     {
         $this->addTab('form_section', array(
-        'name' => Mage::helper('product')->__('Name'),
-        'price' => Mage::helper('product')->__('Price'),
-        'cost' => Mage::helper('product')->__('Cost'),
-        'sku' => Mage::helper('product')->__('Sku'),
-        'tax' => Mage::helper('product')->__('Tax'),
-        'quantity' => Mage::helper('product')->__('Quantity'),
-        'discount' => Mage::helper('product')->__('Discount'),
-        'discount_Mode' => Mage::helper('product')->__('Discount_Mode'),
-        'status' => Mage::helper('product')->__('status'),
-        'createdAt' => Mage::helper('product')->__('created At'),
-        'updatedAt' => Mage::helper('product')->__('updated At'),
+        'label'     => Mage::helper('product')->__('Product Information'),
+        'title'     => Mage::helper('product')->__('Product Information'),
         'content' => $this->getLayout()->createBlock('product/adminhtml_product_index_edit_tab_form')->toHtml(),));
 
         return parent::_beforeToHtml();
