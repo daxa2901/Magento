aded6edcfb20531e4e7f66f10fbbf3d153b7ea7c
daxa2901 <daxadh24@gmail.com>
2022-04-17 15:48:07 +0530

    salesman module

--- app/code/local/Di/Salesman/Block/Adminhtml/Salesman/Index/Edit/Tabs.php
+++ app/code/local/Di/Salesman/Block/Adminhtml/Salesman/Index/Edit/Tabs.php
@@ -13,13 +13,8 @@ class Di_Salesman_Block_Adminhtml_Salesman_Index_Edit_Tabs extends Mage_Adminhtm
     protected function _beforeToHtml()
     {
         $this->addTab('form_section', array(
-        'firstName' => Mage::helper('salesman')->__('First Name'),
-        'lastName' => Mage::helper('salesman')->__('Last Name'),
-        'email' => Mage::helper('salesman')->__('Email'),
-        'mobile' => Mage::helper('salesman')->__('Mobile'),
-        'status' => Mage::helper('salesman')->__('Status'),
-        'createdAt' => Mage::helper('salesman')->__('created At'),
-        'updatedAt' => Mage::helper('salesman')->__('updated At'),
+         'label'     => Mage::helper('salesman')->__('Salesman Information'),
+        'title'     => Mage::helper('salesman')->__('Salesman Information'),
         'content' => $this->getLayout()->createBlock('salesman/adminhtml_salesman_index_edit_tab_form')->toHtml(),));
 
         return parent::_beforeToHtml();
